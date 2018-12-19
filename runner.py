617f3108245b6121e2db1e43b823d5ec26075ebf
chardorn <46007754+chardorn@users.noreply.github.com>
2018-12-19 15:38:06 -0500

    added threading

--- runner.py
+++ runner.py
@@ -3,60 +3,59 @@ import subprocess
 from gpiozero import LED
 import time
 from time import sleep
+import threading
 
+def blinkLED():
+    whlie alarming == True:
+        led.on()
+        time.sleep(0.2)
+        led.off()
+        time.sleep(0.2)
+    
+def checkCube():
+    while alarming == True:
+        print("Checking cube...")
+        result = subprocess.Popen("python -m scripts.label_image --image=test.jpg", stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell = True)
+        out, err = result.communicate()
+        newString = out[36:44]
+        compare = str(newString, "utf-8")
+        if compare == "solved (":
+            alarming = False
+            print("DONE!!!!")
+        
+def takePicture():
+    while alarming == True:
+        print("Taking picture...")
+        os.system("sudo fswebcam -s 2 --no-banner --save test.jpg")
+
+#Set up needed variables
 led = LED(17)
-
 alarmTime = "2056"
 timeForAlarm = False
+led.on()
+alarming = True
 
-
+#Test alarm portion
+#TODO: change so it actually functions
 while timeForAlarm == True:
     currentTime = time.strftime("%H%M")
     if currentTime == alarmTime:
         timeForAlarm = True
 
-led.on()
-
-alarming = True
-
+#Change test.jpeg to an unsolved picture
 os.system("source ./venv/bin/activate")
 os.system("convert unsolvedPic test.jpg")
-time.sleep(5)
 
-while alarming == True :
-    print("STILL GOING")
-    os.system("sudo fswebcam -s 2 --no-banner --save test.jpg")
-    result = subprocess.Popen("python -m scripts.label_image --image=test.jpg", stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell = True)
-    out, err = result.communicate()
-    newString = out[36:44]
-    print("WE OUT HERE")
-    compare = str(newString, "utf-8")
-    if compare == "solved (":
-        alarming = False
-        print("DONE!!!!")
-        led.off()
-    
-        
+t1 = threading.Thread(target=blinkLED)
+t2 = threading.Thread(target=checkCube)
+t3 = threading.Thread(target=takePicture)
 
+t1.start()
+t2.start()
+t3.start()
 
-
-#os.system("cd tensorflow-for-poets-2/tf_files/")
-
-#os.system("ls")
-#os.system("cd tensorflow-for-poets-2")
-#os.system("python -m scripts.label_image --image=test")
-
-
-
-#THIS SHIT (ALMOST) WORKS KINDA
-#result = subprocess.Popen("python -m scripts.label_image --image=test", stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell = True)
-#out, err = result.communicate()
-#newString = out[36:44]
-#print("Confidence of solved: ")
-#print(newString)
-#print(out)
-
-#print(err)
-#print(result.stdout.decode('utf-8'))
-
-#subprocess.run("python -m scripts.label_image --image=tf_files/test")
+t1.join()
+t2.join()
+t3.join()
+led.off()
+print("DONE!")
