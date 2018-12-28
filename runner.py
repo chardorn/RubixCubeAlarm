4d0c806433d0b683673826c15a3b987110be0967
chardorn <46007754+chardorn@users.noreply.github.com>
2018-12-28 16:22:45 -0500

    began threading with GUI

--- runner.py
+++ runner.py
@@ -11,8 +11,8 @@ led = LED(17)
 root = tk.Tk()
 root.configure(background='skyblue')
 
-alarm = "1234"
-alarmTime_ = tk.Label(root, text = "Alarm: " + alarm[0:2] + ":" + alarm[2:4], bg='skyblue', font=('times',18))
+alarmTime = "1234"
+alarm_ = tk.Label(root, text = "Alarm: " + alarmTime[0:2] + ":" + alarmTime[2:4], bg='skyblue', font=('times',18))
 
 entry = tk.Entry(root)
 entry.grid(column=0,row=3)
@@ -24,13 +24,13 @@ def tm(event=None):
     time_.grid(column=0,row=0, padx=5, pady=5)
     root.after(1000,thread)
 
-def alarmTime(event=None):
-    alarmTime_.grid(column=0,row=1, padx=5, pady=5)
+def alarm(event=None):
+    alarm_.grid(column=0,row=1, padx=5, pady=5)
     root.after(1000,thread)
 
 def resetAlarm(event=None):
-    alarm = entry.get()
-    alarmTime_ .config(text = "Alarm:" + alarm[0:2] + ":" + alarm[2:4])
+    alarmTime = entry.get()
+    alarm_ .config(text = "Alarm: " + alarmTime[0:2] + ":" + alarmTime[2:4])
 
 def thread(event=None):
     t = threading.Thread(target=tm)
@@ -38,29 +38,54 @@ def thread(event=None):
 
 def quit(event=None):
     root.destroy()
-
-
-tm()
-alarmTime()
+    
+def blink(event=None):
+    alarming = False
+    while True:
+        currentTime = time.strftime("%H%M")
+        print("Current time: " + currentTime)
+        print("Alarm time: " + alarmTime)
+        if currentTime == alarmTime:
+            alarming = True
+            timeForAlarm = True
+        time.sleep(1)
+        while alarming == True:
+            led.on()
+            time.sleep(0.2)
+            led.off()
+            time.sleep(0.2)
+        
+def takePicture():
+    while alarming == True: #TO DO: change to while alarming = True
+        print("Taking picture...")
+        os.system("sudo fswebcam -s 2 --no-banner --save test.jpg")
+        #result = subprocess.Popen("sudo fswebcam -s 2 --no-banner --save test.jpg", stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell = True)
+        #out, err = result.communicate()
+        time.sleep(3)
+        #os.system("sudo ~/Downloads/usbreset /dev/bus/usb/001/024")
+    
 
 reset_btn = tk.Button(root, text='Reset Alarm', command=resetAlarm)
 reset_btn.config(relief='ridge')
 reset_btn.grid(row=2, column=0, padx=5, pady=5, ipadx=5, ipady=5)
 reset_btn.bind('<Return>', resetAlarm)
 
+tm()
+alarm()
+
+t1 = threading.Thread(target=blink)
+t1.start()
+
+root.mainloop()
+root.after(1000,thread)
+
+
+
+#QUIT BUTTON
 #qt_btn = tk.Button(root, text='quit', command=quit)
 #qt_btn.config(relief='ridge')
 #qt_btn.grid(row=1, column=2, padx=5, pady=5, ipadx=5, ipady=5)
 #qt_btn.bind('<Return>', quit)
 
-root.mainloop()
-while True: #TO DO: change to while truw
-    led.on()
-    print("on")
-    time.sleep(0.2)
-    led.off()
-    print("off")
-    time.sleep(0.2)
 
-#root.after(1000,thread)
 
