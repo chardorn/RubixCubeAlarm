0d645e35010855a7aeaf1fff824563f7e1a02a41
chardorn <charleydorn@gmail.com>
2019-07-06 13:16:15 -0400

    Added fswebcam to runner.test

--- runner_test.py
+++ runner_test.py
@@ -6,12 +6,13 @@ device_name = "USB Camera"
 
 def takePicture():
         print("Taking picture...")
-        os.system("imagesnap test.jpg -d \"" + device_name +"\"")
+        #os.system("imagesnap test.jpg -d \"" + device_name +"\"")
+        os.system("sudo fswebcam -s 2 --no-banner --save test.jpg")
         #result = subprocess.Popen("imagesnap test.jpg -device_name USB Camera", stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell = True)
         #out, err = result.communicate()
         #print(out)
         print("Done!")
-        os.system("python label_image_upgraded.py --image=$HOME/Documents/RubixCubeAlarm/images/test.jpg")
+        #os.system("python label_image_upgraded.py --image=$HOME/Documents/RubixCubeAlarm/images/test.jpg")
 
 #t1 = threading.Thread(target=blinkLED)
 #t2 = threading.Thread(target=checkCube)
